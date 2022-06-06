e97ebf63c837d0518c23c8ca365648d294c80c93
Surasin1991 <surashin.p@gmail.com>
2022-06-06 17:23:35 +0700

    upload

---
+++ Javascript_Homework/Variable/main.js
@@ -0,0 +1,27 @@
+// let a = 20;
+// let b = 50;
+
+// var firstName;
+// var age = 18;
+// age = 50;
+
+// let product = "Apple";
+// let car;
+
+// const animal = "rabbit";
+// const gender = "Male";
+// gender = "Female";
+
+// const a = 20;
+// a = 5;
+
+// let c = 20;
+var c;
+console.log(c);
+{
+  let c = 5;
+  //   console.log("Hello World");
+  console.log(c);
+}
+var c = 20;
+console.log(c);
