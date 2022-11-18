4e15d3d96a4c36a8bb51a1dc35b1feae4afe1279
Surasin1991 <surashin.p@gmail.com>
2022-11-18 13:19:13 +0700

    upload lab6-2

--- React_Homework/react/my-app/src/App.js
+++ React_Homework/react/my-app/src/App.js
@@ -1,49 +1,13 @@
-// import './App.css';
+// lab6-2
 
-function App() {
-  // const style = {
-  //   margin: "12px",
-  //   border: "1px solid black",
-  //   padding: '16px',
-  //   backgroundColor: '#ccc'
-  // }
-  // return (
-  // <div style={style}>
-  //   Hello React
-  // </div>
-  // );
-
-  // const ano = (a, b) => {
-  //   return event => {
-  //     console.log(event);
-  //     console.log(a + b);
-  //   };
-  // };
-
-  return (
-//   <div style={{
-//     margin: "12px", 
-//     color: 'blue',
-//     fontSize: '23px',
-//     border: "1px solid black",
-//     backgroundColor: 'yellow',
-//     borderRadius: '8px'
-//   }}
-//   className="text-center"
-//   >
-//     Hello React App</div>
-// }
-    
-    // <button value="111" onClick={event => console.log(event)}> Click </button>
-  // <input 
-  //   name="username" 
-  //   placeholder="Username" 
-  //   onChange={(event) => console.log(event)}
-  // />
-  <a href="https://shopee.co.th" onClick={(event) => {
-    event.preventDefault();
-    console.log(event)
-  }}>Navigate</a>
+function App() {(
+  <select>
+    <option>Thailand</option>
+    <option>England</option>
+    <option>Chaina</option>
+    <option>Japan</option>
+    <option>Singapore</option>
+  </select>
 )};
 export default App;
 
