c6d95c02205d4d629003ca1a6b27734c1c7f58d1
Surasin1991 <surashin.p@gmail.com>
2022-11-20 22:58:28 +0700

    update lab6-3

--- React_Homework/react/my-app/src/App.js
+++ React_Homework/react/my-app/src/App.js
@@ -1,15 +1,36 @@
 
 function App() {
   const handleChangeCheckBox = (event) => {
-    console.log(`name:${event.target.name} value:${event.target.value} check:${event.target.checked}`)
+    console.log(`
+      name:${event.target.name}
+      value:${event.target.value}
+      check:${event.target.checked}
+    `)
   }
   return (
   <div style={{margin: '3rem'}}>
-    <input type="checkbox" id="Apple" name="Apple" value="iPhone14Pro" onChange={handleChangeCheckBox}></input>
+    <input 
+      type="checkbox" 
+      id="Apple" 
+      name="Apple" 
+      value="iPhone14Pro" 
+      onChange={handleChangeCheckBox}
+    />
     <label htmlFor="apple">iPhone14Pro</label><br></br>
-    <input type="checkbox" id="Samsung" name="Samsung" value="S44UltraMaxPlus" onChange={handleChangeCheckBox}></input>
+    <input 
+      type="checkbox" 
+      id="Samsung" name="Samsung" 
+      value="S44UltraMaxPlus" 
+      onChange={handleChangeCheckBox}
+    />
     <label htmlFor="Samsung">SamsungS44UltraMaxPlus</label><br></br>
-    <input type="checkbox" id="Mi" name="Mi" value="Mi999ProMaxExtraUltraPlusTurbo" onChange={handleChangeCheckBox}></input>
+    <input 
+      type="checkbox" 
+      id="Mi" 
+      name="Mi" 
+      value="Mi999ProMaxExtraUltraPlusTurbo" 
+      onChange={handleChangeCheckBox}
+    />
     <label htmlFor="Mi">Mi999ProMaxExtraUltraPlusTurbo</label>
   </div>
   
