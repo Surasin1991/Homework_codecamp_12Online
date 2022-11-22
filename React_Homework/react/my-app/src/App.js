c027639a5962e78fcc5a8cca7a48c2c6983b531e
Surasin1991 <surashin.p@gmail.com>
2022-11-23 06:20:38 +0700

    finish homework6-4

--- React_Homework/react/my-app/src/App.js
+++ React_Homework/react/my-app/src/App.js
@@ -1,40 +1,21 @@
 
 function App() {
-  const handleChangeCheckBox = (event) => {
-    console.log(`
-      name:${event.target.name}
-      value:${event.target.value}
-      check:${event.target.checked}
-    `)
-  }
+  const ggLink = document.getElementById("gg")
   return (
-  <div style={{margin: '3rem'}}>
-    <input 
-      type="checkbox" 
-      id="Apple" 
-      name="Apple" 
-      value="iPhone14Pro" 
-      onChange={handleChangeCheckBox}
-    />
-    <label htmlFor="apple">iPhone14Pro</label><br></br>
-    <input 
-      type="checkbox" 
-      id="Samsung" name="Samsung" 
-      value="S44UltraMaxPlus" 
-      onChange={handleChangeCheckBox}
-    />
-    <label htmlFor="Samsung">SamsungS44UltraMaxPlus</label><br></br>
-    <input 
-      type="checkbox" 
-      id="Mi" 
-      name="Mi" 
-      value="Mi999ProMaxExtraUltraPlusTurbo" 
-      onChange={handleChangeCheckBox}
-    />
-    <label htmlFor="Mi">Mi999ProMaxExtraUltraPlusTurbo</label>
-  </div>
-  
-)};
+  <a 
+    href="https://google.com"
+    onClick={event => {
+      event.preventDefault();
+      const response = window.confirm('Leave for google?')
+      if (response == true) {
+        window.location.assign('https://google.com')
+      } 
+    }}
+  >
+    google
+  </a>
+  )
+};
 
 export default App;
 
